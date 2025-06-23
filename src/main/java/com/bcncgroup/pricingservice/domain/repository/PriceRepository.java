bc0292f5b821c8d309c4c1a359b7b3e3b1d805f4
rubioponst <trubiopons@gmail.com>
2025-06-23 20:00:04 +0200

    PriceRepository on domain/repository created

--- src/main/java/com/bcncgroup/pricingservice/domain/repository/PriceRepository.java
+++ src/main/java/com/bcncgroup/pricingservice/domain/repository/PriceRepository.java
@@ -1,5 +1,19 @@
 package com.bcncgroup.pricingservice.domain.repository;
 
-public class PriceRepository {
+import com.bcncgroup.pricingservice.domain.model.Price;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface PriceRepository {
+    /**
+     * Finds the applicable price for a given product and brand at a specific application date.
+     *
+     * @param productId the ID of the product
+     * @param brandId the ID of the brand
+     * @param applicationDate the date and time when the price is to be applied
+     * @return an Optional containing the applicable Price if found, or empty if no applicable price exists
+     */
+    Optional<Price> findApplicablePrice(Long productId, Long brandId, LocalDateTime applicationDate);
     
 }
