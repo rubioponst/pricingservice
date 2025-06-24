b7577160d25a0afbc8da623a9a2fcb158891d922
rubioponst <trubiopons@gmail.com>
2025-06-24 10:55:40 +0200

    Refactor PriceEntity annotations and update SQL data entries for consistency

--- src/main/java/com/bcncgroup/pricingservice/adapters/out/entity/PriceEntity.java
+++ src/main/java/com/bcncgroup/pricingservice/adapters/out/entity/PriceEntity.java
@@ -8,10 +8,12 @@ import java.time.LocalDateTime;
 
 import com.bcncgroup.pricingservice.domain.enums.CurrencyType;
 
+@Entity
+@Table(name = "prices")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class PriceEntity {
 
     @Id
